f30416f12a766f1b45dbcc0c7be124c156c249be
Neil Reddy <vreddy2018@pingry.org>
2016-03-01 19:35:55 -0500

    All working.

--- 10character.py
+++ 10character.py
@@ -4,12 +4,17 @@ f.close()
 
 f = open('output.txt', 'w')
 
+count = 0
+while count < len(contents):
+	contents[count] = contents[count][:-1] 
+	count += 1
+
 for x in contents:
 
 	x = x[0:10]
 
 	print(x)
-	
+
 	f.write(x + '\n')
 
 f.close()
