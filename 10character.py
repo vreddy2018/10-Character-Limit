be6a84559cd1408124d0a8e4ba43e67478b42903
Neil Reddy <vreddy2018@pingry.org>
2016-03-03 11:12:01 -0500

    changed the removal of new line to strip function. Added comments

--- 10character.py
+++ 10character.py
@@ -4,15 +4,12 @@ f.close()
 
 f = open('output.txt', 'w')
 
-count = 0
-while count < len(contents):
-	contents[count] = contents[count][:-1] 
-	count += 1
 
 for x in contents:
 
-	x = x[0:10]
-
+	x = x[0:10] #only keeps characters between 0 and 10
+	x = x.strip('\n') #gets rid of \n so that empty lines wont be printed
+	
 	print(x)
 
 	f.write(x + '\n')
